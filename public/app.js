52b81a332c6fe0c571aadad160601c0a12f58c0f
Michael Dunsterville <michael@klientboost.com>
2019-11-19 21:17:02 -0800

    add todo working

--- public/app.js
+++ public/app.js
@@ -1,19 +1,23 @@
 document.getElementById('addTodo').addEventListener('click', e => {
   e.preventDefault()
   let todo = {
-    name: document.getElementById('name').value
+    name: document.getElementById('todo').value
   }
-  axios.post('/todos', todo)
-    .then(res => {
-      let todo = document.createElement('div')
-      todo.innerHTML = `<div class="complete"></div><h5>${todo.name}</h5><button class="delete btn btn-outline-danger">Delete</button>`
-      document.getElementById('todos').append(todo)
-    })
-    .catch(err => console.log(err))
+  // axios.post('/todos', todo)
+  //   .then(res => {
+      let todoItem = document.createElement('div')
+      todoItem.className = 'todoItem'
+      todoItem.innerHTML = `<div class="complete"></div><h5 class="todoName">${todo.name}</h5><button class="delete red darken-4
+       waves-effect waves-light btn-small">Delete</button>`
+      document.getElementById('items').append(todoItem)
+    // })
+    // .catch(err => console.log(err))
 })
+
 
 document.addEventListener('click', e => {
   if(e.target.className === 'complete') {
-    this.nextSibling.classList.toggle('completed')
+    this.classList.toggle('completed')
+    this.nextSibling.classList.toggle('completedStrike')
   }
 })
